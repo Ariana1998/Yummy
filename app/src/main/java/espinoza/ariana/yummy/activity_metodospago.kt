bfdc613e52a4c3d6c70c6a8586be605cc5d9a7e1
Ariana Espinoza <55963646+Ariana1998@users.noreply.github.com>
2021-11-23 19:22:26 -0800

    Actualizaciones

---
+++ app/src/main/java/espinoza/ariana/yummy/activity_metodospago.kt
@@ -0,0 +1,32 @@
+package espinoza.ariana.yummy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioButton
+import android.view.View
+
+class activity_metodospago : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metodospago)
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.opcionefectivo ->
+                    if (checked) {
+                        // Pirates are the best
+                    }
+                R.id.opciontarjeta ->
+                    if (checked) {
+                        // Ninjas rule
+                    }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/espinoza/ariana/yummy/activity_servicioalcliente.kt
@@ -0,0 +1,11 @@
+package espinoza.ariana.yummy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_servicioalcliente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_servicioalcliente)
+    }
+}
